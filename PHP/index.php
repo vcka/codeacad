54c02c613cba566261c078be8e05596e45430bdb
vcka <vcka@ministras.lt>
2018-10-15 13:45:05 +0300

    PHP

--- PHP/index.php
+++ PHP/index.php
@@ -1,13 +1,13 @@
 <?php
-include('simple_html_dom.php');
-// Create DOM from URL or file
-$html = file_get_html('http://www.google.com/');
-
-// Find all images
-foreach($html->find('img') as $element)
-       echo $element->src . '<br>';
-
-// Find all links
-foreach($html->find('a') as $element)
-       echo $element->href . '<br>'; 
+$url = 'http://acid3.acidtests.org';
+$handle = curl_init($url);
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+$html = curl_exec($handle);
+libxml_use_internal_errors(true); // Prevent HTML errors from displaying
+$doc = new DOMDocument();
+$doc->loadHTML($html);
+$link = $doc->getElementsByTagName('a')->item(0);
+echo 'Link text: ' . $link->nodeValue;
+echo '<br />';
+echo 'Link URL: ' . $link->getAttribute('href');
 ?>
