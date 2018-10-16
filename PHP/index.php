0b064693a66b74937a621d09d318be0263c93b94
vcka <vcka@ministras.lt>
2018-10-16 14:00:42 +0300

    Test

--- PHP/index.php
+++ PHP/index.php
@@ -1,19 +1,22 @@
 <?php
 
+//header("Refresh:1");
 date_default_timezone_set('Europe/Vilnius');
 $data =  date('Y-m-d  H:i:s');
+$m = date('m');
+$d = date('d');
+$title = 'As, '.date(D).' ir PHP';
+$h1 = 'Vytautas - HTML ir PHP jau nuo '. date('Y').' metu';
 
 $html = '<!DOCTYPE html>
 <html lang="en" dir="ltr">
-
 <head>
-  <meta charset="utf-8">
-   <title>HW-CA</title>
+<meta charset="utf-8">
+<title>'.$title.'</title>
 </head>
-
 <body>
-    <h1>Vytautas - HTML asas!</h1>
-    <p>Moku sukurti statini turini. Atidaryta '.$data.'</p>
+<h1>'.$h1.'</h1>
+<p>Viskas prasidejo '.$m.' meneso ' .$d.' diena!</p>
 </body>';
-echo $html;
+die ($html);
 ?>
