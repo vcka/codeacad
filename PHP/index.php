9047be8888a0ed0d7723e67a17b5ed06747a18bf
vcka <vcka@ministras.lt>
2018-10-16 14:24:48 +0300

    Last

--- PHP/index.php
+++ PHP/index.php
@@ -1,23 +1,19 @@
 <?php
-
 //header("Refresh:1");
 date_default_timezone_set('Europe/Vilnius');
-$data =  date('Y-m-d  H:i:s');
-$m = date('m');
-$d = date('d');
-$title = 'As, '.date(D).' ir PHP';
-$h1 = 'Vytautas - HTML ir PHP jau nuo '. date('Y').' metu';
-$tommorow = date('Y-m-d', strtotime("next day"));
+$title = 'PHP lydes ir ryt '.date('Y-m-d', strtotime("next day"));
+$h1 = 'Vytautas - PHP su manim buvo ir '.date('H', strtotime("-1 hour")).' valanda!';
+$paragraph = date('Y', strtotime("next year")).' ne uz kalnu';
 
 $html = '<!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
 <meta charset="utf-8">
-<title>PHP lydes ir ryt '.$tommorow.'</title>
+<title>'.$title.'</title>
 </head>
 <body>
 <h1>'.$h1.'</h1>
-<p>Viskas prasidejo '.$m.' meneso ' .$d.' diena!</p>
+<p>'.$paragraph.'</p>
 </body>';
 die ($html);
 ?>
