1d97b76661581a710c9d1c6c13982e3ae7cfd0f9
vcka <vcka@ministras.lt>
2018-10-17 12:38:50 +0300

    3 day

--- PHP/index.php
+++ PHP/index.php
@@ -1,36 +1,14 @@
 <?php
 header("Refresh:1");
-//VLN laiko juosta
-date_default_timezone_set('Europe/Vilnius');
-/*Ir visa
- * Kita matematika
- */
-$vaikai = rand(1, 5);
-$random = rand(strtotime("Oct 16 2018"), strtotime("Oct 16 2028"));
-$random2 = rand(strtotime("Oct 16 2021"), strtotime("Oct 16 2121"));
-$title = 'PHP lydes ir '.date('Y-m-d',$random);
-$h1 = 'Vytautas - Galbut turesiu '.$vaikai.' vaiku!';
-$paragraph = 'D. Trump`as nebus prezidentu '.date('Y-m-d',$random2);
-$r = rand(0, 255);
-$g = rand(0, 255);
-$b = rand(0, 255);
-$f = rand(1, 100);
-$top = rand(5, 95);
-$side = rand(5, 95);
-
-$html = '<!DOCTYPE html>
-<!--Pagrindine nesamone
-<p>Nieko nera</p>
--->
+//$dice = rand(1, 6);
+$dice = rand(1, 6).'.png';$html = '<!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
 <meta charset="utf-8">
 <title>'.$title.'</title>
 </head>
-<body style="background:rgb('.$r.','.$g.','.$b.')">
-<h1 style="font-size:'.$f.'px">'.$h1.'</h1>
-<p style="color: rgb('.$b.','.$g.','.$r.')">'.$paragraph.'</p>
-<div style="height: 50px; width: 50px; background-color: #555; position: absolute; top: '.$top.'%; right: '.$side.'%;padding: 10px;">
+<body>
+<div style="height:600px; width: 55%; background-image: url('.$dice.'); background-repeat: no-repeat; margin:0 auto;">
 </body>';
 die ($html);
 ?>
