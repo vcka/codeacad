f6a59aecb33889c4954dbc0d41c9e35e0119eb24
vcka <vcka@ministras.lt>
2018-10-17 13:16:00 +0300

    Extra 10

--- PHP/index.php
+++ PHP/index.php
@@ -1,14 +1,41 @@
 <?php
-header("Refresh:1");
-//$dice = rand(1, 6);
-$dice = rand(1, 6).'.png';$html = '<!DOCTYPE html>
+
+//header("Refresh:1");
+$sec = date('s');
+$id = '';
+if (is_int($sec / 2)) {
+    //print("Lyginis: $sec");
+    $id = 'square';
+} else {
+    //print("Nelyginis: $sec");
+    $id = 'circle';
+}
+$html = '<!DOCTYPE html>
 <html lang="en" dir="ltr">
+
 <head>
-<meta charset="utf-8">
-<title>'.$title.'</title>
+  <meta charset="utf-8">
+  <title>HW-CA</title>
+  <style>
+    #circle {
+      width: 50px;
+      height: 50px;
+      -webkit-border-radius: 25px;
+      -moz-border-radius: 25px;
+      border-radius: 25px;
+      background: red;
+    } 
+    #square {
+  height: 50px;
+  width: 50px;
+  background-color: #555;
+}
+</style>
+
 </head>
+
 <body>
-<div style="height:600px; width: 55%; background-image: url('.$dice.'); background-repeat: no-repeat; margin:0 auto;">
+<div id="'.$style.'">'.$sec.'</div>
 </body>';
-die ($html);
+die($html);
 ?>
